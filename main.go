492675bbf97e8b9b97a5500ec620ee3b95d03569
Artem Roma <aroma@mirantis.com>
2017-03-14 17:06:56 +0200

    Final assignment version

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
+//I've determined given problem as an instance of Traveling Salesman Problem,
+//since we need to find the shortest (least expensive) route that allows us to visit
+//every station.
+
+//I've decided not to use Nearest Neighbour Algorithm (brute force) but to go with one of
+//heuristic algorithms, namely Christofides' Algorithm. But unfortunately my lack of
+//graph theory knowledge prevented me from completing the implementation. Still I've
+//managed to complete initial steps for it.
+
+//Christofides' Algorithm can be broken to following steps:
+//1. find minimum spanning tree
+//2. get subset of odd order vertices
+//3. create minimum-weight perfect matching with those vertices
+//4. combine that with MST
+//5. find Eulerian tour on the combined graph
+//6. remove from the tour repeated vertices to get a path - this will be the answer
+
+//In given task not only the cost but also time of travel should be regarded,
+//thus edges of the graph have two weights, which I don't yet know to incorporate
+//into Christofides' Algorithm, but in case of brute force such weight's processing
+//might be done
+
 package main
 
 import (
@@ -26,9 +48,11 @@ func (bc ByCost) Less(i, j int) bool { return bc[i].Cost < bc[j].Cost }
 type empty struct{}
 
 type djSetNode struct {
-	Parent *djSetNode
+	Parent      *djSetNode
+	VertexOrder int
 }
 
+//Disjoint set data type for Kruskal's Algorithm
 func (sn *djSetNode) Find() *djSetNode {
 	if sn.Parent == nil {
 		return sn
@@ -65,8 +89,6 @@ func main() {
 	for _, d := range data {
 		var err error
 		number, err := strconv.Atoi(d[0])
-		//depStation, err := strconv.Atoi(d[1])
-		//arrStation, err := strconv.Atoi(d[2])
 		cost, err := strconv.ParseFloat(d[3], 32)
 
 		if err != nil {
@@ -92,11 +114,9 @@ func main() {
 		}
 	}
 
-	fmt.Println(stSet)
-
 	sort.Sort(ByCost(trains))
-	fmt.Println(trains[0:2])
 
+	//find MST using Kruskal's Algoritm
 	spanningTree := []Train{}
 	for _, t := range trains {
 		depStNode := stSet[t.DepStation]
@@ -108,6 +128,10 @@ func main() {
 		if depStNodeParent != arrStNodeParent {
 			spanningTree = append(spanningTree, t)
 			depStNode.Union(arrStNode)
+
+			//calculate order
+			depStNode.VertexOrder += 1
+			arrStNode.VertexOrder += 1
 		}
 	}
 
@@ -115,4 +139,17 @@ func main() {
 	fmt.Println("#### spanning tree ---> ", spanningTree)
 
 	//find odd vertices
+	oddVert := []string{}
+	for key, val := range stSet {
+		if val.VertexOrder%2 != 0 {
+			oddVert = append(oddVert, key)
+		}
+	}
+
+	//After this step minimum weight perfect matching must be found
+	//from initial graph. This could be done using using the Hungarian Algorithm
+	//for bipartide graph; but this phrase is all that I can say about these concepts:)
+
+	//The final steps are finding of Euler's tour using Fleury's Algorithm (don't know
+	//much about it either) and removing vertices to get a path.
 }
